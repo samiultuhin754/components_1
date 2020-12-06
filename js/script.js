bcdbe81c9552a437e21c6753790131cf2d964fd4
samiultuhin754 <73142944+samiultuhin754@users.noreply.github.com>
2020-12-06 14:52:32 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
+<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
